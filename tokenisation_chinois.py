c644a132c739b1d73c0829b9cfb81fef982f7b90
unknown <alicesisi.lin@gmail.com>
2026-04-02 10:26:31 +0200

    Ajout tokenisation

--- tokenisation_chinois.py
+++ tokenisation_chinois.py
@@ -1,7 +1,39 @@
 import jieba
+from pathlib import Path
 
-corpus = open("corpus.txt", "r", encoding="utf-8").read()
+def parcourir_arborescence(sample: str) -> list[str]:
+    """Parcours récursif avec pathlib via iterdir()."""
+    p = Path(sample)
+    fichiers_trouves = []
+    if p.is_file() and p.suffix.lower() == ".txt":
+        fichiers_trouves.append(str(p))
+    elif p.is_dir():
+        for element in p.iterdir():
+            fichiers_trouves.extend(parcourir_arborescence(element))
+    return fichiers_trouves
+
+
+def reader(files: list[str]) -> str:
+    """
+    Lecture des fichiers et concaténation du contenu
+    """
+    if not files:
+        return ""
+
+    corpus = ""
+
+    for file in files:
+        with open(file, "r", encoding="utf-8") as f:
+            corpus += f.read() + "\n"  # lire le contenu
+
+    return corpus
+
+
+chemin = "docs/"
+corpus = reader(parcourir_arborescence(chemin))
 
 tokens = jieba.lcut(corpus)
-for tok in tokens :
+
+for tok in tokens:
     print(tok)
+print(len(tokens))
