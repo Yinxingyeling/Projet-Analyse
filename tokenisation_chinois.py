90957f0c96d27f72e939fbad7461b4e36bbdbb46
luhongrui1428 <luhongrui1428@gmail.com>
2026-04-09 09:41:38 +0200

    Clean des bruits du webpage

--- tokenisation_chinois.py
+++ tokenisation_chinois.py
@@ -1,5 +1,6 @@
 import jieba
 from pathlib import Path
+import re
 
 def parcourir_arborescence(sample: str) -> list[str]:
     """Parcours récursif avec pathlib via iterdir()."""
@@ -28,11 +29,44 @@ def reader(files: list[str]) -> str:
 
     return corpus
 
+def nettoyage(corpus:str)->str:
+    corpus = re.sub(r"简介\([^)]*\)","",corpus)
+    corpus = re.sub(r"目录\([^)]*\)", "", corpus)
+
+    corpus = re.sub(r"\b\d+\s*/\s*\d+\b", "", corpus)
+    corpus = re.sub(r"\b\d+\s* /\s* \d+\b", "", corpus)
+
+    bruits = [
+        r"加大减小",
+        r"跳转",
+        r"字号",
+        r"夜间",
+        r"跳章",
+        r"点击进入",
+        r"上一页",
+        r"下一页",
+        r"点击呼出",
+        r"功能菜单"
+    ]
+    for p in bruits:
+        corpus = re.sub(p, "", corpus)
+
+
+    lines = [line.strip() for line in corpus.splitlines() if line.strip()]
+    nettoye = []
+    seen = set()
+    for line in lines:
+        if line not in seen:
+            nettoye.append(line)
+            seen.add(line)
+
+    return "\n".join(nettoye)
 
 chemin = "docs/"
 corpus = reader(parcourir_arborescence(chemin))
+corpus_net = nettoyage(corpus)
 
-tokens = jieba.lcut(corpus)
+tokens = jieba.lcut(corpus_net)
 
 for tok in tokens:
     print(tok)
